7a21f9efb0c82592e5f5e2e421b3717c947c0b08
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2025-01-24 03:52:34 +0530

    Adding Changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,146 +1,64 @@
-import { Box, Typography } from "@mui/material";
-import Search from "../components/Search";
-import HeroContact from "../components/HeroContact";
-import PartnersCompany from "./PartnersCompany";
+import React from "react";
+import { Box, Button, Typography, Grid } from "@mui/material";
+import LocationSelector from "./LocationSelector";
+import ContactOptions from "./ContactOptions";
 
 const HeroSection = () => {
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "100vh",
-        position: "relative",
-        overflow: "hidden",
-        marginTop:"64px",
-        backgroundImage: 'url("https://res.cloudinary.com/dkoezhi9u/image/upload/v1737480349/hero_pjv4yv.svg")',
-        backgroundSize: "contain",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "top center",
-        top: "-50px",
-        padding: 0,
-        zIndex: 0,
+        padding: { xs: 2, md: 4 },
+        margin: { xs: "64px 0px", md: "72px 0px" },
+        padding: { xs: "24px 24px", md: "48px 84px" },
+        backgroundImage:
+          "url('https://res.cloudinary.com/dkoezhi9u/image/upload/v1737480349/hero_pjv4yv.svg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
-      <Box
-        sx={{
-          width: "579px",
-          height: "156px",
-          position: "absolute",
-          top: "200px",
-          left: "200px",
-          color: "white",
-        }}
-      >
-        <Typography
-          variant="h1"
+      <Grid container spacing={4} alignItems="center">
+        <Grid item xs={12} md={6}>
+          <Typography variant="h2" sx={{ fontWeight: "bold", marginBottom: 2 }}>
+            Smart Living Style
+            <br />
+            For Smart People
+          </Typography>
+          <Typography variant="body1" sx={{ marginBottom: 4 }}>
+            A better, healthier you! We are the best studio in the area. Our
+            studio provides you with personal training and nutrition plans, as
+            well as a variety of classes at the studio. A unique experience!
+          </Typography>
+
+          <LocationSelector />
+
+          <ContactOptions sx={{ marginTop: 4 }} />
+        </Grid>
+
+        <Grid
+          item
+          xs={12}
+          md={6}
           sx={{
-            fontSize: "56px",
-            fontWeight: "bold",
-            lineHeight: 1.2,
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          Smart Living Style
-          <br />
-          For Smart people
-        </Typography>
-      </Box>
-
-      <Box
-        sx={{
-          width: "559px",
-          height: "63px",
-          position: "absolute",
-          top: "372px",
-          left: "200px",
-          color: "white",
-        }}
-      >
-        <Typography
-          variant="body1"
-          sx={{
-            fontSize: "18px",
-            fontWeight: "normal",
-            lineHeight: 1.5,
-          }}
-        >
-          A better, healthier you! We are the best studio in the area. Our
-          studio provides you with a personal training and nutrition plan, as
-          well as a variety of classes at the studio. A unique experience!
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "200px",
-          right: "160px",
-          width: "580px",
-          height: "480px",
-          borderRadius: "32px",
-          overflow: "hidden",
-          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-        }}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "0",
-            left: "0",
-            right: "0",
-            bottom: "0",
-            borderRadius: "32px",
-            border: "8px solid rgba(255, 255, 255, 0.2)",
-            pointerEvents: "none",
-            zIndex: 1,
-          }}
-        />
-        <Box
-          component="img"
-          sx={{
-            width: "100%",
-            height: "100%",
-            borderRadius: "32px",
-            objectFit: "cover",
-            boxSizing: "border-box",
-          }}
-          src="https://res.cloudinary.com/dkoezhi9u/image/upload/v1737480362/heroimg_dbtrqs.jpg"
-          alt="Hero"
-        />
-      </Box>
-
-      <Box
-        sx={{
-          width: "918px",
-          height: "102px",
-          position: "absolute",
-          top: "500px",
-          left: "200px",
-          zIndex: 2,
-        }}
-      >
-        <Search />
-      </Box>
-
-      {/* Hero Contact Section */}
-      <Box
-        sx={{
-          position: "relative",
-          top: "602px", // Adjust position as needed
-          left: "200px",
-          zIndex: 2,
-        }}
-      >
-        <HeroContact />
-      </Box>
-      {/* Partner Companies Section */}
-      <Box
-        sx={{
-          position: "relative",
-          top: "730px", // Adjust position as needed
-          zIndex: 2,
-        }}
-      >
-        <PartnersCompany />
-      </Box>
+          <Box
+            sx={{
+              backgroundImage:
+                "url('https://res.cloudinary.com/dkoezhi9u/image/upload/v1737480362/heroimg_dbtrqs.jpg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: "5%",
+              width: "80%",
+              paddingTop: "60%",
+              border: "12px solid rgba(250,250,250,0.3)",
+              overflow: "hidden",
+              boxShadow: "0 4px 15px rgba(0, 0, 0, 0.1)",
+            }}
+          ></Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
